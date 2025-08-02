678bfe728291f24967a21d39040e14f116113f81
Jack Saenz <jack.p.saenz@gmail.com>
2025-08-02 08:20:32 -0500

    Added Fish

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,16 +1,14 @@
-local wezterm = require 'wezterm'
+local os = require("os")
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+-- Style
 config.enable_wayland = false
-
 config.enable_tab_bar = false
--- config.window_padding = {
---    left = '0cell',
---    right = '0cell',
---    top = '0.5cell',
---    bottom = '0.5cell',
--- }
 config.window_background_opacity = 0.0
+
+-- Shell
+config.default_prog = { "/sbin/fish", "-l" }
 
 return config
