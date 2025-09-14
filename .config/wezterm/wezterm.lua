b0f7c7eed87585e3cd0cd12337b18e6e5e65b024
Jack Saenz <jack.p.saenz@gmail.com>
2025-09-14 00:39:08 -0500

    Start of waybar config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,6 +8,10 @@ config.enable_wayland = false
 config.enable_tab_bar = false
 config.window_background_opacity = 0.0
 
+config.colors = {
+	foreground = "white",
+}
+
 -- Shell
 config.default_prog = { "/sbin/fish", "-l" }
 
